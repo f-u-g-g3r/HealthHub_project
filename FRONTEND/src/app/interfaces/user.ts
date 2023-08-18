25c437308c9c934810dd196d99df8ab67fbb75c4
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2023-08-18 20:28:03 +0300

    .

---
+++ FRONTEND/src/app/interfaces/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: number;
+    firstname: string;
+    lastname: string;
+    dateOfBirth: string;
+    gender: string;
+    address: string;
+    email: string;
+    phone: string;
+    password: string;
+    medCardID: number;
+}
