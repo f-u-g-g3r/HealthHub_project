3b62ecfde5871d421afe7ab59d4d8b027b3cf69b
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2023-08-19 15:08:21 +0300

    Created user service

--- FRONTEND/src/app/services/user/user.service.ts
+++ FRONTEND/src/app/services/user/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { User } from 'src/app/interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private serverUrl = environment.serverUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public all():  Observable<User[]> {
+    return this.http.get<User[]>(`${this.serverUrl}/users`);
+  }
 }
