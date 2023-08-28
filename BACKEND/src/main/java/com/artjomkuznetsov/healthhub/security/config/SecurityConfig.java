175621603817992685e98395781684d06db68502
Artjom Kuznetsov <artjom.kuznetsov1733@gmail.com>
2023-08-28 18:37:33 +0300

    Update SecurityConfig.java

--- BACKEND/src/main/java/com/artjomkuznetsov/healthhub/security/config/SecurityConfig.java
+++ BACKEND/src/main/java/com/artjomkuznetsov/healthhub/security/config/SecurityConfig.java
@@ -12,12 +12,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Autowired
-    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider) {
         this.jwtAuthFilter = jwtAuthFilter;
         this.authenticationProvider = authenticationProvider;
     }
